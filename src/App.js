37745b1a24db56e78ed41d9e512b152612def71c
scott.joness <scottjonez00@gmail.com>
2023-10-18 13:47:43 -0700

    added inputs and values and css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [education, setEducation] = useState('');
+  const [workExperience, setWorkExperience] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='wrapper'>
+      <div className='leftSide'>
+        <div>
+          <label>Name</label>
+          <input className={'nameDisplay'} onChange={
+              (e) => {
+                setName(e.target.value)
+              }
+            } 
+          />
+        </div>
+        <div>
+          <label>Email</label>
+          <input className={'emailDisplay'} onChange={
+              (e) => {
+                setEmail(e.target.value)
+              }
+            }
+          />
+        </div>
+        <div>
+          <label>Phone Number</label>
+          <input onChange={
+              (e) => {
+                setPhoneNumber(e.target.value)
+              }
+            }
+          />
+        </div>
+        <div>
+          <label>Education</label>
+          <input onChange={
+              (e) => {
+                setEducation(e.target.value)
+              }
+            }
+            />
+        </div>
+        <div>
+          <label>Work Experience</label>
+          <input onChange={
+              (e) => {
+                setWorkExperience(e.target.value)
+              }
+            }
+          />
+        </div>
+      </div>
+      <div className='rightSide'>
+        <div className='value'>{name}</div>
+        <div className='value'>{email}</div>
+        <div className='value'>{phoneNumber}</div>
+        <div className='value'>{education}</div>
+        <div className='value'>{workExperience}</div>
+      </div>
     </div>
   );
 }
