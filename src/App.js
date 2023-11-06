f03aac76cd12245470b2923c7c0bdcad9d17a0a4
scott.joness <scottjonez00@gmail.com>
2023-11-06 11:41:02 -0800

    stuff

--- src/App.js
+++ src/App.js
@@ -8,12 +8,65 @@ function App() {
   const [education, setEducation] = useState('');
   const [workExperience, setWorkExperience] = useState('');
 
-  return (
-    <div id='wrapper'>
-      <div className='leftSide'>
+  const [displayName, setDisplayName] = useState('');
+  const [displayEmail, setDisplayEmail] = useState('');
+  const [displayPhoneNumber, setDisplayPhoneNumber] = useState('');
+  const [displayEducation, setDisplayEducation] = useState('');
+  const [displayWorkExperience, setDisplayWorkExperience] = useState('');
+
+  const [contactEditRender, setContactEditRender] = useState(false);
+  const [educationEditRender, setEducationEditRender] = useState(false);
+  const [workExperienceEditRender, setWorkExperienceEditRender] = useState(false);
+
+  const submitContactButtonPress = () => {
+    const contactInputData = {
+      name: name,
+      email: email,
+      phoneNumber: phoneNumber,
+    }
+    setDisplayName(name);
+    setDisplayEmail(email);
+    setDisplayPhoneNumber(phoneNumber);
+
+    setContactEditRender(true);
+  }
+
+  const submitEducationButtonPress = () => {
+    const educationInputData = {
+    education: education,
+    }
+    setDisplayEducation(education);
+
+    setEducationEditRender(true);
+  }
+
+  const submitWorkExperienceButtonPress = () => {
+    const workExperienceInputData = {
+    workExperience: workExperience,
+    }
+    setDisplayWorkExperience(workExperience);
+
+    setWorkExperienceEditRender(true);
+  }
+
+  const editContactButtonPress = () => {
+    setContactEditRender(false);
+  }
+  const editEducationButtonPress = () => {
+    setEducationEditRender(false);
+  }
+  const editWorkExperienceButtonPress = () => {
+    setWorkExperienceEditRender(false);
+  }
+
+  const renderContactInfo = () => {
+    if (contactEditRender){
+      return <button onClick={editContactButtonPress}>Edit</button>
+    } else {
+      return (<div>
         <div>
           <label>Name</label>
-          <input className={'nameDisplay'} onChange={
+          <input value={name} className={'nameDisplay'} onChange={
               (e) => {
                 setName(e.target.value)
               }
@@ -22,7 +75,7 @@ function App() {
         </div>
         <div>
           <label>Email</label>
-          <input className={'emailDisplay'} onChange={
+          <input value={email} className={'emailDisplay'} onChange={
               (e) => {
                 setEmail(e.target.value)
               }
@@ -31,7 +84,7 @@ function App() {
         </div>
         <div>
           <label>Phone Number</label>
-          <input onChange={
+          <input value={phoneNumber} className={'phoneNumberDisplay'} onChange={
               (e) => {
                 setPhoneNumber(e.target.value)
               }
@@ -39,33 +92,90 @@ function App() {
           />
         </div>
         <div>
+        </div>
+        <button onClick={submitContactButtonPress}>Submit</button>
+      </div>)
+    }
+  }
+
+  const renderEducationInfo = () => {
+    if (educationEditRender){
+      return <button onClick={editEducationButtonPress}>Edit</button>
+    } else {
+      return (<div>     
+        <div>
           <label>Education</label>
-          <input onChange={
+          <input value={education} className={'educationDisplay'} onChange={
               (e) => {
                 setEducation(e.target.value)
               }
             }
             />
         </div>
+        <button onClick={submitEducationButtonPress}>Submit</button>
+            </div>)
+         
+    }
+
+  }
+
+  const renderWorkExperienceInfo = () => {
+    if (workExperienceEditRender){
+      return <button onClick={editWorkExperienceButtonPress}>Edit</button>
+    } else {
+      return (<div>     
         <div>
           <label>Work Experience</label>
-          <input onChange={
+          <input value={workExperience} className={'workExperienceDisplay'} onChange={
               (e) => {
                 setWorkExperience(e.target.value)
               }
             }
-          />
+            />
         </div>
+        <button onClick={submitWorkExperienceButtonPress}>Submit</button>
+            </div>)
+         
+    }
+
+  }
+  return (
+    <div id='wrapper'>
+      <div className='leftSide'>
+        <div className='contactWrapper'>
+          <div className='contactInfo'>
+            <div className='contactInfoHeader'><label>Contact Info</label></div>
+              {renderContactInfo()}
+          </div>
+        </div>
+        <div className='educationWrapper'>
+            <div className='educationInfo'>
+              <div className='educationInfoHeader'><label>Education Info</label></div>
+            {renderEducationInfo()}
+            </div>
+          </div>
+          <div className='workExperienceWrapper'>
+            <div className='workExperienceInfo'>
+              <div className='workExperienceInfoHeader'><label>Work Experience Info</label></div>
+            {renderWorkExperienceInfo()}
+            </div>
+          </div>
       </div>
       <div className='rightSide'>
-        <div className='value'>{name}</div>
-        <div className='value'>{email}</div>
-        <div className='value'>{phoneNumber}</div>
-        <div className='value'>{education}</div>
-        <div className='value'>{workExperience}</div>
+        <div className='nameValue'>{displayName}</div>
+        <div className='value'>{displayEmail}</div>
+        <div className='value'>{displayPhoneNumber}</div>
+        <div className='value'>{displayEducation}</div>
+        <div className='value'>{displayWorkExperience}</div>
+      </div>
+     
+      
+      <div> 
+        
       </div>
     </div>
-  );
-}
 
+  );
+
+}
 export default App;
